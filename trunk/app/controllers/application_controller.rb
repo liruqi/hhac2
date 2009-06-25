2b122578b6e49e99d9a2217a82237c07c1a88aaf
physacco <physacco@0a8ba2a8-608e-11de-92c7-cf56be1892c0>
2009-06-25 03:15:59 +0000

    Big progress

--- trunk/app/controllers/application_controller.rb
+++ trunk/app/controllers/application_controller.rb
@@ -14,4 +14,14 @@ class ApplicationController < ActionController::Base
       redirect_to :controller => :login, :action => :index
     end
   end
+
+  def current_user
+    @current_user = User.find_by_id session[:user_id]
+  end
+
+  def logged_in?
+    @current_user != nil
+  end
+
+  helper_method :current_user, :logged_in?
 end
--- trunk/app/controllers/movies_controller.rb
+++ trunk/app/controllers/movies_controller.rb
@@ -25,14 +25,17 @@ class MoviesController < ApplicationController
 
   # GET /movies/new
   # GET /movies/new.xml
-  def new
-    @movie = Movie.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @movie }
-    end
-  end
+  #def new
+  #  if current_user
+  #    @movie = Movie.new
+  #    respond_to do |format|
+  #      format.html # new.html.erb
+  #    # format.xml  { render :xml => @movie }
+  #    end
+  #  else
+  #    redirect_to '/404.html'
+  #  end
+  #end
 
   # GET /movies/1/edit
   def edit
@@ -41,20 +44,24 @@ class MoviesController < ApplicationController
 
   # POST /movies
   # POST /movies.xml
-  def create
-    @movie = Movie.new(params[:movie])
-
-    respond_to do |format|
-      if @movie.save
-        flash[:notice] = 'Movie was successfully created.'
-        format.html { redirect_to(@movie) }
-        format.xml  { render :xml => @movie, :status => :created, :location => @movie }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @movie.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
+  #def create
+  #  if current_user
+  #    @movie = Movie.new(params[:movie])
+  #   
+  #    respond_to do |format|
+  #      if @movie.save
+  #        flash[:notice] = 'Movie was successfully created.'
+  #        format.html { redirect_to(@movie) }
+  #        format.xml  { render :xml => @movie, :status => :created, :location => @movie }
+  #      else
+  #        format.html { render :action => "new" }
+  #        format.xml  { render :xml => @movie.errors, :status => :unprocessable_entity }
+  #      end
+  #    end
+  #  else
+  #    redirect_to '/404.html'
+  #  end
+  #end
 
   # PUT /movies/1
   # PUT /movies/1.xml
@@ -77,6 +84,7 @@ class MoviesController < ApplicationController
   # DELETE /movies/1.xml
   def destroy
     @movie = Movie.find(params[:id])
+    File.delete @movie.path
     @movie.destroy
 
     respond_to do |format|
@@ -97,5 +105,48 @@ class MoviesController < ApplicationController
       path404 = File.join File.dirname(__FILE__), "../../public/404.html"
       render :file => path404, :status => 404
     end
+  end
+
+  def upload
+    if current_user
+      @movie = Movie.new
+    else
+      redirect_to '/404.html'
+    end
+  end
+
+  def save
+    if current_user
+      begin
+        movie = Movie.new params[:movie]
+        raise "Title cannot be empty." if (movie.title =~ /^\s*$/) != nil
+
+        file = params[:file]  # Tempfile实例
+        raise "File was not uploaded." if file == nil
+
+        begin
+          fname = Time.now.strftime("%Y%m%d%H%M%S")+"_"+file.original_filename
+          f = File.new "#{RAILS_ROOT}/public/upload/#{fname}", 'wb'
+          f.write file.read
+
+          movie.path = f.path
+          movie.user_id = current_user.id
+          movie.save
+        rescue
+          raise "Failed to save data."
+        end
+
+        redirect_to movie
+      rescue
+        render :text => "Upload failed.\nException: #{$!.message}\n", :status => 500
+      end
+    else
+      render :text => "Upload failed.\n403 Forbidden", :status => 403
+    end
+  end
+
+  def info
+    @movie = Movie.find_by_id params[:id]
+    render :partial => "info"
   end
 end
