703216a58a9a06c5a48ec63368bfc4fad3860d58
liruqi <liruqi@0a8ba2a8-608e-11de-92c7-cf56be1892c0>
2009-06-29 10:32:01 +0000

    deploy again,

---
+++ trunk/deploy.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/sh
+sudo apt-get install ruby irb rdoc
+
+wget http://rubyforge.org/frs/download.php/57643/rubygems-1.3.4.tgz
+tar -xvf rubygems-1.3.4.tgz
+cd rubygems-1.3.4.tgz
+sudo ruby setup.rb
+ln -s /usr/bin/gem1.8 /usr/bin/gem
+
+sudo apt-get install openssl libopenssl-ruby
+
+
+gem install rails --include-dependencies
+#gem install mongrel
+
+
+svn checkout https://hhac2.googlecode.com/svn/trunk/ hhac2 
+cd hhac2
+
+mysql -uroot -pgogogo -e "CREATE USER hhac@'localhost' IDENTIFIED BY 'iamharmless';"
+mysql -uroot -p -e "CREATE DATABASE IF NOT EXISTS hhac2"
+mysql -uroot -p -e "GRANT ALL PRIVILEGES ON hhac2.* TO hhac"
+
+rake db:migrate
+
+./script/server
+
